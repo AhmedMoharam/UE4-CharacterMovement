85938c18bd5eab5f32f69153a9e9c481aa4c7d8f
Moharam <a.moharam@yahoo.com>
2017-12-09 23:50:06 +0200

    Orient character Yaw to controller Yaw (On Character moving)

---
+++ DarkSuperManProject/Source/DarkSuperManProject/Public/SuperManMovementComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SuperManMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class DARKSUPERMANPROJECT_API USuperManMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
